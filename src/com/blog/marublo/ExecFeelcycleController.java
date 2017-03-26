aa561184057db52e049654ef0ce1324cadd277a9
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2017-03-26 14:59:11 +0900

    b-monster

--- src/com/blog/marublo/ExecFeelcycleController.java
+++ src/com/blog/marublo/ExecFeelcycleController.java
@@ -318,6 +318,8 @@ public class ExecFeelcycleController {
 			//int debugCount = driver.findElements(By.cssSelector("#g-console > li:nth-child(1) > button > span")).size();
 			//System.out.println("debugCount:"+debugCount + "個");
 			driver.manage().window().maximize();
+            List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+            System.out.println("The total number of iframes are " + iframeElements.size());
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			js.executeScript("document.getElementsByClassName('btn').click();");
 			//driver.findElement(By.cssSelector("#g-console > li:nth-child(1) > button > span")).click();
