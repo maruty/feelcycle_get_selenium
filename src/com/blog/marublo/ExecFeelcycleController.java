3e0768eb56f8f9558370a33827fb9611110befdc
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2017-10-29 00:08:41 +0900

    UA

--- src/com/blog/marublo/ExecFeelcycleController.java
+++ src/com/blog/marublo/ExecFeelcycleController.java
@@ -26,7 +26,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
