43007d4373918f6613ce121d81334b6d104736bf
YanagisawaYutaka <maruty@YanagisawaYutakanoMacBook-puro.local>
2018-04-22 13:22:16 +0900

    再起動対応など

---
+++ src/com/blog/marublo/JenkinsInfo.java
@@ -0,0 +1,30 @@
+package com.blog.marublo;
+
+public class JenkinsInfo {
+	String id;
+	String pass;
+	
+	JenkinsInfo(){
+		
+	}
+	
+	JenkinsInfo(String id, String pass){
+		this.id = id;
+		this.pass = pass;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+}
