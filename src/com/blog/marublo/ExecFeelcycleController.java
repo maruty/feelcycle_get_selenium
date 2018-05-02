1657388ec492af285fd2a5e721c7b3f20862e1b7
柳澤 雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2018-05-02 12:52:47 +0900

    firefox高速化

--- src/com/blog/marublo/ExecFeelcycleController.java
+++ src/com/blog/marublo/ExecFeelcycleController.java
@@ -25,7 +25,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
@@ -96,59 +95,61 @@ public class ExecFeelcycleController {
 		//jenkins のビルドの　MAVEN_OPTISに-DargLine="-Dwebdriver.chrome.driver=/opt/chromedriver/chromedriver"
 		//を設定している
 
-
-		FirefoxProfile profile = new FirefoxProfile();
-
-		/*
-		profile.setPreference("network.http.pipelining", true);
-		profile.setPreference("network.http.pipelining.firstrequest", true);
-		profile.setPreference("network.http.pipelining.maxrequests", 8);
-		profile.setPreference("network.http.max-connections", 32);
-		profile.setPreference("network.http.max-connections-per-server", 8);
-		profile.setPreference("network.http.max-persistent-connections-per-proxy", 8);
-		profile.setPreference("network.http.max-persistent-connections-per-server", 4);
-		profile.setPreference("nglayout.initialpaint.delay", 0);
-		profile.setPreference("content.notify.backoffcount", 5);
-		profile.setPreference("plugin.expose_full_path", true);
-		profile.setPreference("ui.submenuDelay", 0);
-		profile.setPreference("content.interrupt.parsing", true);
-		profile.setPreference("content.max.tokenizing.time", 2250000);
-		profile.setPreference("content.notify.interval", 750000);
-		profile.setPreference("content.notify.ontimer", true);
-		profile.setPreference("content.switch.threshold", 750000);
-		profile.setPreference("browser.cache.memory.capacity", 65536)
-		*/
-
-
-
-		profile.setPreference("network.http.pipelining", true);
-		profile.setPreference("network.http.pipelining.firstrequest", true);
-		profile.setPreference("network.http.pipelining.maxrequests", 8);
-		profile.setPreference("network.http.max-connections", 32);
-		profile.setPreference("network.http.max-connections-per-server", 8);
-		profile.setPreference("network.http.max-persistent-connections-per-proxy", 8);
-		profile.setPreference("network.http.max-persistent-connections-per-server", 4);
-		profile.setPreference("nglayout.initialpaint.delay", 0);
-		profile.setPreference("content.notify.backoffcount", 5);
-		profile.setPreference("plugin.expose_full_path", true);
-		profile.setPreference("ui.submenuDelay", 0);
-		profile.setPreference("content.interrupt.parsing", true);
-		profile.setPreference("content.max.tokenizing.time", 2250000);
-		profile.setPreference("content.notify.interval", 750000);
-		profile.setPreference("content.notify.ontimer", true);
-		profile.setPreference("content.switch.threshold", 750000);
-		profile.setPreference("browser.cache.memory.enable", true);
-		profile.setPreference("browser.cache.memory.capacity", -1);
-
-		//browser.cache.memory.enableをtrueに
-		profile.setPreference("browser.sessionstore.interval", 6000000);
-		profile.setPreference("network.http.spdy.enabled.http2", false);
-
+		FirefoxOptions options = new FirefoxOptions();
+		//options.addPreference(key, value)
+		options.addPreference("browser.chrome.image_icons.max_size", 0);
+		options.addPreference("browser.display.show_image_placeholders", false);
+		options.addPreference("browser.download.manager.addToRecentDocs", false);
+		options.addPreference("browser.formfill.expire_days", 30);
+		options.addPreference("browser.link.open_newwindow.restriction", 0);
+		options.addPreference("browser.overlink-delay", 0);
+		options.addPreference("browser.search.openintab", true);
+		options.addPreference("browser.sessionhistory.max_entries", 7);
+		options.addPreference("browser.sessionhistory.max_total_viewers", 1);
+		options.addPreference("browser.sessionstore.interval", 300000);
+		options.addPreference("browser.sessionstore.max_windows_undo", 1);
+		options.addPreference("browser.tabs.animate", false);
+		options.addPreference("browser.tabs.closeWindowWithLastTab", false);
+		options.addPreference("browser.tabs.insertRelatedAfterCurrent", false);
+		options.addPreference("browser.tabs.loadBookmarksInBackground", true);
+		options.addPreference("browser.tabs.warnOnCloseOtherTabs", false);
+		options.addPreference("browser.taskbar.lists.enabled", false);
+		options.addPreference("browser.taskbar.lists.frequent.enabled", false);
+		options.addPreference("browser.taskbar.lists.tasks.enabled", false);
+		options.addPreference("browser.taskbar.previews.max", 1);
+		options.addPreference("browser.urlbar.filter.javascript", false);
+		options.addPreference("browser.urlbar.formatting.enabled", false);
+		options.addPreference("browser.urlbar.trimURLs", false);
+		options.addPreference("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+		options.addPreference("dom.popup_maximum", 1);
+		options.addPreference("extensions.getAddons.cache.enabled", false);
+		options.addPreference("extensions.pocket.enabled", false);
+		options.addPreference("general.smoothScroll.mouseWheel.durationMaxMS", 150);
+		options.addPreference("general.smoothScroll.mouseWheel.durationMinMS", 150);
+		options.addPreference("geo.enabled", false);
+		options.addPreference("gfx.direct2d.force-enabled", true);
+		options.addPreference("gfx.font_rendering.cleartype.always_use_for_content", true);
+		options.addPreference("keyword.enabled", false);
+		options.addPreference("layers.acceleration.force-enabled", true);
+		options.addPreference("layout.css.report_errors", false);
+		options.addPreference("layout.word_select.eat_space_to_next_word", false);
+		options.addPreference("media.autoplay.enabled", false);
+		options.addPreference("mousewheel.default.delta_multiplier_y", 150);
+		options.addPreference("network.dnsCacheExpiration", 86400);
+		options.addPreference("network.http.request.max-start-delay", 0);
+		options.addPreference("network.http.spdy.enabled.http2", false);
+		options.addPreference("network.http.speculative-parallel-limit", 0);
+		options.addPreference("privacy.trackingprotection.enabled", true);
+		options.addPreference("view_source.wrap_long_lines", true);
 
 
-    	FirefoxOptions options = new FirefoxOptions();
-    	options.setProfile(profile);
+    	//options.setProfile(profile);
     	WebDriver driver = new FirefoxDriver(options);
+
+
+
+
+
 
     	///FirefoxProfile profile = new FirefoxProfile();
 
